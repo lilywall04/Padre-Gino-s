fe683ac56a9ef125ebc561ee4c56ed04faa967fc
Lily Wallace <s1554872@student.mcckc.edu>
2025-09-12 01:16:28 -0500

    1st update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,35 @@
 import js from "eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
+import { react } from "@babel/types";
 
 /** atype {import('eslint).linter.Config[]} */
 export default [
-    js.configs.recommended,
-    {
-        files: ["**/*js", "**/*.jsx"],
-        languageOptions: {
-            globals: {...globals.broweser, ...globals.node},
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true,
-                }
-            }
-        }
+  js.configs.recommended,
+  {
+    ...reactPlugin.config.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
-    prettier
-]
+  },
+  reactPlugin.configs.flat["jsx-runtime"],
+  {
+    files: ["**/*js", "**/*.jsx"],
+    languageOptions: {
+      globals: { ...globals.broweser, ...globals.node },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      "react/no-unescaped-entites": "off",
+      "react/prop-types": "off",
+    },
+  },
+  prettier,
+];
