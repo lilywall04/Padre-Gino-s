dfd4f2723322a0b5da72d171290512e1864c5b37
Lily Wallace <s1554872@student.mcckc.edu>
2025-09-12 10:39:19 -0500

    API Additions

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,21 @@ const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza name = "Pepperoni" description="pep, cheese, n stuff" />
-      <Pizza name = "Hawaiian" description="ham, cheese, n pineapple" />
-      <Pizza name = "Americano" description="'merica frick ya" />
+      <Pizza
+        name="Pepperoni"
+        description="pep, cheese, n stuff"
+        image={"/public/pizzas/pepperoni.webp"}
+      />
+      <Pizza
+        name="Hawaiian"
+        description="ham, cheese, n pineapple"
+        image={"/public/pizzas/hawaiian.webp"}
+      />
+      <Pizza
+        name="Americano"
+        description="'merica frick ya"
+        image={"/public/pizzas/big_meat.webp"}
+      />
     </div>
   );
 };
