0cef5386383f5c4958a74c447c747b4c2d17b769
Lily Wallace <s1554872@student.mcckc.edu>
2025-09-25 16:21:26 -0500

    Previous Orders Query

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import js from "eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
 import reactPlugin from "eslint-plugin-react";
-import { react } from "@babel/types";
+import pluginQuery from "@tanstack/eslint-plugin-query"
 
 /** atype {import('eslint).linter.Config[]} */
 export default [
@@ -16,6 +16,7 @@ export default [
     },
   },
   reactPlugin.configs.flat["jsx-runtime"],
+  ...pluginQuery.configs['flat/recommended'],
   {
     files: ["**/*js", "**/*.jsx"],
     languageOptions: {
